3df89f7674b59db925322234a0b93bc46d5d176b
Stephan Conrad <stephan@conrad.pics>
2018-02-28 14:40:03 +0100

    sync

--- root/usr/bin/entrypoint.sh
+++ root/usr/bin/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 check_is_func(){
-    type $1|grep "is a shell function" > /dev/null
+	type $1
+    type "$1" 2> /dev/null|grep "is a shell function" > /dev/null 2>&1
     RC=$?
     if [[ $RC -eq 0 ]]; then
         exit 0
@@ -16,14 +17,16 @@ if [[ -d /etc/docker.entrypoint.d ]]; then
     done
 fi
 
+echo $CALL_unifictl
+
 CMD=$1
 CMD_VAR="CALL_${CMD}"
 if [[ ${#@} -gt 0 ]]; then
     shift
 fi
 
-if [[ check_is_func $CMD_VAR ]]; then
-    $CMD_VAR $@
+if check_is_func "\$${CMD_VAR}"; then
+    echo $CMD_VAR $@
 else
-    $CMD $@
+    echo $CMD $@
 fi
