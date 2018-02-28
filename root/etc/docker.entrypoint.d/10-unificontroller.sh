ff89083f9e2b72c0507bf7658d34d3f800cb7037
Stephan Conrad <stephan.conrad@coop.ch>
2018-02-28 14:28:04 +0100

    generic entrypoint script

--- root/etc/docker.entrypoint.d/10-unificontroller.sh
+++ root/etc/docker.entrypoint.d/10-unificontroller.sh
@@ -6,12 +6,4 @@ unifictl(){
     java -jar lib/ace.jar $*
 }
 
-
-
-if [[ "$ENTRYPOINT_FUNCS" == "" ]]; then
-    ENTRYPOINT_FUNCS="unifictl:unifictl"
-else
-    ENTRYPOINT_FUNCS="${ENTRYPOINT_FUNCS} unifictl:unifictl"
-fi
-
-export ENTRYPOINT_FUNCS
+export CALL_unifictl="unifictl"
--- root/usr/bin/entrypoint.sh
+++ root/usr/bin/entrypoint.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+check_is_func(){
+    type $1|grep "is a shell function" > /dev/null
+    RC=$?
+    if [[ $RC -eq 0 ]]; then
+        exit 0
+    else 
+        exit 1
+    fi
+}
+
 if [[ -d /etc/docker.entrypoint.d ]]; then
     for FILE in "/etc/docker.entrypoint.d/*.sh"; do
         . ${FILE}
     done
 fi
 
-for CALL_FUNC in $ENTRYPOINT_FUNCS; do
-    echo $CALL_FUNC
-done
+CMD=$1
+CMD_VAR="CALL_${CMD}"
+if [[ ${#@} -gt 0 ]]; then
+    shift
+fi
+
+if [[ check_is_func $CMD_VAR ]]; then
+    $CMD_VAR $@
+else
+    $CMD $@
+fi
